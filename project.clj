14c71ed9f921d7677ff1b4911c4e54b99fb73ece
Arnout Roemers <roemers@zorgdomein.nl>
2020-06-11 20:23:05 +0200

    Release 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject functionalbytes/lifted "1.0.0-SNAPSHOT"
+(defproject functionalbytes/lifted "1.0.0"
   :description "Lift Clojure functions into protocols."
   :url "https://github.com/aroemers/lifted"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
